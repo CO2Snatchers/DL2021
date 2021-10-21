8aef2e171cc3d687b830030e52151509527fa817
CO2Snatchers <co2snatchers@gmail.com>
2021-10-21 12:06:13 -0500

    initialcommit

---
+++ info.js
@@ -0,0 +1,61 @@
+info = {
+    "0" : {
+        "name": "pAM4787-CA2",
+        "gene-expression" : "(CA2 - carbon anhydrase - CO2 fixation)",
+        "organism" : "Chlamydomonas",
+        "description" : "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of the CA2 carbon anhydrase gene.",
+        "route" : "static/media/images/plasmid0.jpeg"
+    },
+    "1" : {
+        "name": "pAM4787-PEPC",
+        "gene-expression" : "(PEPC - phosphoenolpyruvate carboxylase - CO2 fixation)",
+        "organism" : "Chlamydomonas",
+        "description": "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of the PEPC phosphoenolpyruvate carboxylase gene.",
+        "route" : "static/media/images/plasmid1.jpeg"
+    },
+    "2": {
+        "name": "pAM4787-CA2-PEPC",
+        "gene-expression" : ["Gene 1: (indA - carbon anhydrase - CO2 fixation)",
+        "Gene 2: (PEPC - phosphoenolpyruvate carboxylase - CO2 fixation)"],
+        "organism" : "Chlamydomonas",
+        "description": "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of both CA2 and PEPC genes. It is expected that the combination of both genes will enhance the fixation of CO2. A multicistronic expression is assembled within the cassette by inserting the F2A self-cleaving peptide between both genes.",
+        "route" : "static/media/images/plasmid2.jpeg"
+    },
+    "3": {
+        "name": "pCambia1300-Pac1-indA-indB-indC-pecM",
+        "gene-expression" : ["Gene 1: (indA - glycosynthase (?) - indigoidine production)",
+        "Gene 2: (indB - phosphatase (?) - indigoidine production)",
+        "Gene 3: (indC - non-ribosomal peptide synthetase - indigoidine production)",
+        "Gene 4: (pecM - metabolite exporter - indigoidine cell exportation)"],
+        "organism" : "Chlamydomonas",
+        "description": "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of all genes involved in the production and exportation of indigoidine. A multicistronic expression is assembled within the cassette by inserting the F2A self-cleaving peptide between genes.",
+        "route" : "static/media/images/plasmid3.jpeg"
+    },
+    "4": {
+        "name": "pCambia1300-Pac1-indA-indB-pecM",
+        "gene-expression" : ["Gene 1: (indA - glycosynthase (?) - indigoidine production)",
+        "Gene 2: (indB - phosphatase (?) - indigoidine production)",
+        "Gene 3: (pecM - metabolite exporter - indigoidine cell exportation)"],
+        "organism" : "Chlamydomonas",
+        "description": "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of genes involved in the production (indA and indB) and exportation (pecM) of indigoidine. A multicistronic expression is assembled within the cassette by inserting the F2A self-cleaving peptide between genes.",
+        "route" : "static/media/images/plasmid4.jpeg"
+    },
+    "5": {
+        "name": "pCambia1300-Pac1-indA-indC-pecM",
+        "gene-expression" : ["Gene 1: (indA - glycosynthase (?) - indigoidine production)",
+        "Gene 2: (indC - non-ribosomal peptide synthetase - indigoidine production)",
+        "Gene 3: (pecM - metabolite exporter - indigoidine cell exportation)"],
+        "organism" : "Chlamydomonas",
+        "description": "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of all genes involved in the production (indA and indC) and exportation (pecM) of indigoidine. A multicistronic expression is assembled within the cassette by inserting the F2A self-cleaving peptide between genes.",
+        "route" : "static/media/images/plasmid5.jpeg"
+    },
+    "6": {
+        "name": "pCambia1300-Pac1-indB-indC-pecM",
+        "gene-expression" : ["Gene 1: (indB - phosphatase (?) - indigoidine production)",
+        "Gene 2: (indC - non-ribosomal peptide synthetase - indigoidine production)",
+        "Gene 3: (pecM - metabolite exporter - indigoidine cell exportation)"],
+        "organism" : "Chlamydomonas",
+        "description": "Cyanobacteria synthetic plasmid including the Chlamydomonas reinhardtii high expression Psad promoter and the RBCS2 terminator to render the expression of all genes involved in the production (indB and indC) and exportation (pecM) of indigoidine. A multicistronic expression is assembled within the cassette by inserting the F2A self-cleaving peptide between genes.",
+        "route" : "static/media/images/plasmid6.jpeg"
+    }
+}
